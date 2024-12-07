6736158faa40640911c118339da1e8531998a23f
fanimoreiraneto <fanineto1@gmail.com>
2024-12-07 00:23:09 +0000

    updated movies directory mainly

--- build/scripts/jersey-with-embedded-jetty.bat
+++ build/scripts/jersey-with-embedded-jetty.bat
@@ -17,7 +17,7 @@
 @if "%DEBUG%"=="" @echo off
 @rem ##########################################################################
 @rem
-@rem  Gradle startup script for Windows
+@rem  jersey-with-embedded-jetty startup script for Windows
 @rem
 @rem ##########################################################################
 
@@ -28,13 +28,13 @@ set DIRNAME=%~dp0
 if "%DIRNAME%"=="" set DIRNAME=.
 @rem This is normally unused
 set APP_BASE_NAME=%~n0
-set APP_HOME=%DIRNAME%
+set APP_HOME=%DIRNAME%..
 
 @rem Resolve any "." and ".." in APP_HOME to make it shorter.
 for %%i in ("%APP_HOME%") do set APP_HOME=%%~fi
 
-@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
-set DEFAULT_JVM_OPTS="-Xmx64m" "-Xms64m"
+@rem Add default JVM options here. You can also use JAVA_OPTS and JERSEY_WITH_EMBEDDED_JETTY_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS=
 
 @rem Find java.exe
 if defined JAVA_HOME goto findJavaFromJavaHome
@@ -68,22 +68,22 @@ goto fail
 :execute
 @rem Setup the command line
 
-set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+set CLASSPATH=%APP_HOME%\lib\jersey-with-embedded-jetty.jar;%APP_HOME%\lib\slf4j-simple-1.7.25.jar;%APP_HOME%\lib\slf4j-api-1.7.25.jar;%APP_HOME%\lib\jersey-container-jetty-http-2.27.jar;%APP_HOME%\lib\jetty-servlet-9.4.6.v20170531.jar;%APP_HOME%\lib\jetty-security-9.4.6.v20170531.jar;%APP_HOME%\lib\jetty-server-9.4.7.v20170914.jar;%APP_HOME%\lib\jersey-container-servlet-core-2.27.jar;%APP_HOME%\lib\jersey-server-2.27.jar;%APP_HOME%\lib\jersey-media-json-jackson-2.27.jar;%APP_HOME%\lib\jersey-hk2-2.27.jar;%APP_HOME%\lib\sqlite-jdbc-3.47.0.0.jar;%APP_HOME%\lib\bcprov-jdk18on-1.79.jar;%APP_HOME%\lib\javax.servlet-api-3.1.0.jar;%APP_HOME%\lib\jetty-http-9.4.7.v20170914.jar;%APP_HOME%\lib\jetty-io-9.4.7.v20170914.jar;%APP_HOME%\lib\jersey-client-2.27.jar;%APP_HOME%\lib\jersey-media-jaxb-2.27.jar;%APP_HOME%\lib\jersey-common-2.27.jar;%APP_HOME%\lib\jersey-entity-filtering-2.27.jar;%APP_HOME%\lib\javax.ws.rs-api-2.1.jar;%APP_HOME%\lib\hk2-locator-2.5.0-b42.jar;%APP_HOME%\lib\hk2-api-2.5.0-b42.jar;%APP_HOME%\lib\hk2-utils-2.5.0-b42.jar;%APP_HOME%\lib\javax.annotation-api-1.2.jar;%APP_HOME%\lib\javax.inject-2.5.0-b42.jar;%APP_HOME%\lib\validation-api-1.1.0.Final.jar;%APP_HOME%\lib\jetty-util-9.4.7.v20170914.jar;%APP_HOME%\lib\jetty-continuation-9.4.7.v20170914.jar;%APP_HOME%\lib\jackson-module-jaxb-annotations-2.8.10.jar;%APP_HOME%\lib\jackson-databind-2.8.10.jar;%APP_HOME%\lib\jackson-annotations-2.8.10.jar;%APP_HOME%\lib\osgi-resource-locator-1.0.1.jar;%APP_HOME%\lib\jackson-core-2.8.10.jar;%APP_HOME%\lib\aopalliance-repackaged-2.5.0-b42.jar;%APP_HOME%\lib\javassist-3.22.0-CR2.jar;%APP_HOME%\lib\javax.inject-1.jar
 
 
-@rem Execute Gradle
-"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %*
+@rem Execute jersey-with-embedded-jetty
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %JERSEY_WITH_EMBEDDED_JETTY_OPTS%  -classpath "%CLASSPATH%" com.lumistream.jersey.JerseyApplication %*
 
 :end
 @rem End local scope for the variables with windows NT shell
 if %ERRORLEVEL% equ 0 goto mainEnd
 
 :fail
-rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem Set variable JERSEY_WITH_EMBEDDED_JETTY_EXIT_CONSOLE if you need the _script_ return code instead of
 rem the _cmd.exe /c_ return code!
 set EXIT_CODE=%ERRORLEVEL%
 if %EXIT_CODE% equ 0 set EXIT_CODE=1
-if not ""=="%GRADLE_EXIT_CONSOLE%" exit %EXIT_CODE%
+if not ""=="%JERSEY_WITH_EMBEDDED_JETTY_EXIT_CONSOLE%" exit %EXIT_CODE%
 exit /b %EXIT_CODE%
 
 :mainEnd
--- src/main/java/com/lumistream/jersey/movies/MovieOperations.java
+++ src/main/java/com/lumistream/jersey/movies/MovieOperations.java
@@ -5,34 +5,116 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MovieOperations {
-    private static String SQL = "jdbc:sqlite:LumiStream/src/main/databases/movies.db";
-
+   	//private static String SQL = "jdbc:sqlite:LumiStream/src/main/databases/movies.db";
+	private static String SQL = "jdbc:sqlite:src/main/databases/movies.db";
+	
     public static Movie getMovie(String title){
         Movie movie = null;
-        String query = "SELECT (description, genre, release_year, poster_url, video_url_360p, video_url_1080p, duration, rating) from movies where title = " + title;
-
+        //String query = "SELECT (description, genre, release_year, poster_url, video_url_360p, video_url_1080p, duration, rating) from movies where title = " + title;
+	    String query = "SELECT movie_id, title, description, genre, release_year, poster_url, video_url_360p, video_url_1080p, duration, rating FROM movies WHERE title = ?";
+        
         try (Connection conn = DriverManager.getConnection(SQL)) {
             PreparedStatement pstm = conn.prepareStatement(query);
+            pstm.setString(1, title); // parameterized query to prevent SQL injection
             ResultSet res = pstm.executeQuery();
-            String description = res.getString("description");
-            String genre = res.getString("genre");
-            Integer release_year = res.getInt("release_year");
-            String poster_url = res.getString("poster_url");
-            String video_url_360p = res.getString("video_url_360p");
-            String video_url_1080p = res.getString("video_url_1080p");
-            Double duration = res.getDouble("duration");
-            Double rating = res.getDouble("reting");
-            movie = new Movie(title, description, genre, release_year, poster_url, video_url_360p, video_url_1080p, duration, rating);
+                
+            if (res.next()) { 
             
-        } catch (SQLException e) {
-            System.out.println(e.getMessage());
-        }
-        return movie;
-    }
+                System.out.println("Movie found: " + title);
 
-    public void addMovie(String title, String description, String genre, Integer release_year, String poster, Double duration, Double rating){
+                int movie_id = res.getInt("movie_id");
+                String description = res.getString("description");
+                if(description == null) description = "";
+
+                String genre = res.getString("genre");
+                if(genre == null) genre = "Unknown";
+
+                Integer release_year = res.getInt("release_year");
+                if (res.wasNull()) release_year = 0; 
+                
+                String poster_url = res.getString("poster_url");
+                if (poster_url == null) poster_url = "default_poster_url";
+
+                String video_url_360p = res.getString("video_url_360p");
+                if(video_url_360p == null) video_url_360p = "default_video_360p_url";
+                
+                String video_url_1080p = res.getString("video_url_1080p");
+                if(video_url_1080p == null) video_url_1080p =  "default_video_1080p_url";
+
+                Double duration = res.getDouble("duration");
+                if (res.wasNull()) duration = 0.0; 
+                
+                Double rating = res.getDouble("rating");
+                if (res.wasNull()) rating = 0.0; 
+
+
+                movie = new Movie(movie_id, title, description, genre, release_year, poster_url, video_url_360p, video_url_1080p, duration, rating);
+            } else {
+                System.out.println("No movie found with title: " + title);
+            }
+                
+            } catch (SQLException e) {
+                System.out.println("Error fetching movie: " + e.getMessage());
+            }
+            return movie;
+        }
+
+    public static List<Movie> getListMovies(int limit){
+        List<Movie> movies = new ArrayList<>();
+        
+        String query = "SELECT movie_id, title, description, genre, release_year, poster_url, video_url_360p, video_url_1080p, duration, rating FROM movies LIMIT ?";
+        
+        try (Connection conn = DriverManager.getConnection(SQL)) {
+            PreparedStatement pstm = conn.prepareStatement(query);
+            
+            pstm.setInt(1, limit); // parameterized query to prevent SQL injection
+            ResultSet res = pstm.executeQuery();
+                
+            while (res.next()) { 
+            
+                int movie_id = res.getInt("movie_id");
+                String title = res.getString("title");
+                String description = res.getString("description");
+                if(description == null) description = "";
+
+                String genre = res.getString("genre");
+                if(genre == null) genre = "Unknown";
+
+                Integer release_year = res.getInt("release_year");
+                if (res.wasNull()) release_year = 0; 
+                
+                String poster_url = res.getString("poster_url");
+                if (poster_url == null) poster_url = "default_poster_url";
+
+                String video_url_360p = res.getString("video_url_360p");
+                if(video_url_360p == null) video_url_360p = "default_video_360p_url";
+                
+                String video_url_1080p = res.getString("video_url_1080p");
+                if(video_url_1080p == null) video_url_1080p =  "default_video_1080p_url";
+
+                Double duration = res.getDouble("duration");
+                if (res.wasNull()) duration = 0.0; 
+                
+                Double rating = res.getDouble("rating");
+                if (res.wasNull()) rating = 0.0; 
+
+
+                movies.add(new Movie(movie_id, title, description, genre, release_year, poster_url, video_url_360p, video_url_1080p, duration, rating));
+            }
+                
+        } catch (SQLException e) {
+            System.out.println("Error fetching movie: " + e.getMessage());
+        }
+        return movies;
+    }    
+
+    public void addMovie(Integer movie_id, String title, String description, String genre, Integer release_year,
+                        String poster_url, String video_url_360p, String video_url_1080p,
+                        Double duration, Double rating){
         
         //add movie entry into the database, the file into the ningx, and convert it into low resolution
     }
